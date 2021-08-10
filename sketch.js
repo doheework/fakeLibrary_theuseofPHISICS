1caddda5d425d4e604b19f2b4b29bf67f122b780
Your Namedo <you@example.com>
2021-08-10 22:10:59 +0900

    10.08

---
+++ sketch.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".울버린").mouseover(function(){
+    $(".울버린i").show();
+    $(".울버린i").stop().animate({top:100})
+    });
+    $(".울버린").mouseout(function(){
+    $(".울버린i").stop().animate({top:600})
+    $(".울버린i").hide();
+    });
+    });
+
+$(document).ready(function(){
+        $(".수업").mouseover(function(){
+        $(".수업i").show();
+        $(".수업i").stop().animate({top:"50%"})
+        });
+        $(".수업").mouseout(function(){
+        $(".수업i").stop().animate({top:1000})
+        $(".수업i").hide();
+        });
+        });
+
+$(document).ready(function(){
+    $(".시즌").mouseover(function(){
+    $(".시즌i").show();
+    $(".시즌i").stop().animate({top:0})
+    });
+    $(".시즌").mouseout(function(){
+    $(".시즌i").stop().animate({top:500})
+    $(".시즌i").hide();
+    });
+    });
